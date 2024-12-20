e2a4196417b0705708512139a877cd77caff4b45
Matfolkarn <70518913+Matfolkarn@users.noreply.github.com>
2024-12-20 14:13:19 +0100

    Create generat_parantheses.py

---
+++ python_solutions/generat_parantheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        result = []
+        stack = []
+        
+        def backtrack(openn, closen):
+            if openn == closen == n:
+                result.append("".join(stack))
+                return
+            
+            if openn < n:
+                stack.append("(")
+                backtrack(openn + 1, closen)
+                stack.pop()
+            if closen < openn:
+                stack.append(")")
+                backtrack(openn, closen +1)
+                stack.pop()
+
+        backtrack(0,0) 
+        return result
